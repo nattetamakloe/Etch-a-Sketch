89d12c54be8a884736096be314927245b556743c
N8DGR8 <38409384+nattetamakloe@users.noreply.github.com>
2025-11-25 16:15:35 +0100

    Create setSquare function
    Functioon that creates squares based on input number

--- scipt.js
+++ scipt.js
@@ -1,6 +1,12 @@
 const container = document.querySelector("#container");
+function setSquare(number) {
+    for (let index = 0; index < number; index++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square");
+        container.appendChild(square);
 
-const square = document.createElement("div");
-square.setAttribute("class", "square");
-container.appendChild(square);
+    }
+}
+
+setSquare(1)
 
