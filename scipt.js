ddf5bc0754a7eaf77e3724777180a5e8ae592d0f
N8DGR8 <38409384+nattetamakloe@users.noreply.github.com>
2025-11-26 16:43:16 +0100

    Add eventlistener funcrtion

--- scipt.js
+++ scipt.js
@@ -1,16 +1,27 @@
 const container = document.querySelector("#container");
+const newBtn = document.querySelector("#newBtn");
+newBtn.addEventListener('click', () => {
+    let newSquare = prompt('How many squares per side?');
+    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+
+    setSquare(newSquare);
+    
+})
 function setSquare(number) {
-    for (let index = 0; index < number; index++) {
+    for (let index = 0; index < Math.pow(number, 2); index++) {
         const square = document.createElement("div");
         square.setAttribute("class", "square");
         square.addEventListener('mouseenter', () => {
             square.style.background = "black";
         })
-        
+
         container.appendChild(square);
 
     }
 }
 
-setSquare(256)
+setSquare(16)
 
