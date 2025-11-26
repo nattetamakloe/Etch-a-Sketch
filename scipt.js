8d0469ef26df7db5da063504e58ac42050fbbebd
N8DGR8 <38409384+nattetamakloe@users.noreply.github.com>
2025-11-26 15:20:42 +0100

    Add hover effekt in js

--- scipt.js
+++ scipt.js
@@ -3,6 +3,12 @@ function setSquare(number) {
     for (let index = 0; index < number; index++) {
         const square = document.createElement("div");
         square.setAttribute("class", "square");
+        square.addEventListener('mouseenter', () => {
+            square.style.background = "black";
+        })
+        square.addEventListener('mouseleave', () => {
+            square.style.background = "white";
+        })
         container.appendChild(square);
 
     }
